cab8c53fc62bd05ca5d5f4b0401bc6ab5148559c
Alex Bzdel <ab913@duke.edu>
2023-02-15 19:18:05 +0000

    added some helper bash scripts

--- bash_cli/check_active.sh
+++ bash_cli/check_active.sh
@@ -1,8 +1,14 @@
 # run aws sagemaker list-models in a loop and save each "ModelName" to local file
 # repeat same step for endpoint (check if config is deleted with it)
 
+# parse json for model and endpoint names
 aws sagemaker list-models | jq ".Models[0].ModelName" > modelname.txt
 aws sagemaker list-endpoints | jq ".Endpoints[0].EndpointName" > endpointname.txt
+aws sagemaker list-endpoint-configs | jq ".EndpointConfigs[0].EndpointConfigName" > endpointconfigname.txt
 
-# parse models.json for modelname
-
+# export above to environment variables
+# may be a better way to eliminate the multiple steps here
+# must source this script in the delete_resources.sh script
+export model=$(cat modelname.txt)
+export endpt=$(cat endpointname.txt)
+export endptconfig=$(cat endpointconfigname.txt)
--- bash_cli/delete_resources.sh
+++ bash_cli/delete_resources.sh
@@ -1,5 +1,18 @@
+source check_active.sh
+
+# take out quotes from environment variables, store in temp variables
+model_temp=$(echo $model | tr -d '"')
+endpt_temp=$(echo $endpt | tr -d '"')
+endptconfig_temp=$(echo $endptconfig | tr -d '"')
+
+
+# load in model and endpoint names
 echo "deleting resources..."
-aws sagemaker delete-model --model-name $(cat modelname.txt)
+aws sagemaker delete-model --model-name $model_temp
 echo "model deleted"
-aws sagemaker delete-endpoint --endpoint-name $(cat endpointname.txt)
+aws sagemaker delete-endpoint --endpoint-name $endpt_temp
 echo "endpoint deleted"
+aws sagemaker delete-endpoint-config --endpoint-config-name $endptconfig_temp
+echo "endpoint config deleted"
+
+echo "all resources successfully deleted!"
